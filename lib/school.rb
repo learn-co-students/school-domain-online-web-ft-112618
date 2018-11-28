9ae6b657fe4b10df3e5924c44cff432f370a7210
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-28 19:36:10 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require "pry"
+
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(s_name, grade)
+    self.roster[grade] = [] unless self.roster[grade].kind_of?(Array)
+    self.roster[grade] << s_name
+  end
+
+  def grade(g)
+    self.roster[g]
+  end
+
+  def sort
+    self.roster.each do |grade, s_array|
+      s_array.sort!
+    end
+  end
+
+end
